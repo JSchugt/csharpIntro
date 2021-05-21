d73881da4e0f8f6a5789017bf84da94f492b2877
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-05-21 11:28:49 -0500

    First Commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+Console.WriteLine("What is your first name?");
+string name = System.Console.ReadLine();
+Console.WriteLine("What is your first name?");
+string lastName = System.Console.ReadLine();
+if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(lastName))
+{
+    Console.WriteLine("Fine, don't say hello");
+}
+else
+{
+    for (int i = name.Length - 1; i >= 0; i--)
+    {
+        Console.Write(name[i]);
+    }
+    Console.Write(" ");
+    for (int i = lastName.Length - 1; i >= 0; i--)
+    {
+        Console.Write(lastName[i]);
+    }
+}
